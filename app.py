261f3dc5109e0185fde60977df2f0f770b838b4a
JaredGaastra <JGaastra2@Gmail.com>
2023-05-24 16:31:52 -0700

    adding tags

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from models import db, connect_db, Users, Posts
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -67,14 +68,92 @@ def delete_user(user_id):
 def add_post(user_id):
     """adding a post for specific user"""
     user = Users.query.get_or_404(user_id)
-
+    posts = Users.query.get_or_404(user_id).user_posts
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
 
-        new_post = Posts(title=title, content=content, user_id=user_id)
+        new_post = Posts(title=title, content=content, user_id=user_id, created_at=datetime.now() )
         db.session.add(new_post)
         db.session.commit()
 
-        return redirect(url_for('user_detail', user_id=user_id))
-    return render_template('user_post.html', user=user)
+        return redirect(url_for('user_detail', user_id=user_id, posts=posts))
+    return render_template('user_post.html', user=user , posts=posts)
+
+@app.route('/posts/<int:post_id>')
+def post_detail(post_id):
+    """post detail"""
+    post = Posts.query.get_or_404(post_id)
+    return render_template('post_detail.html', post=post)
+
+@app.route('/posts/<int:post_id>/edit', methods=['GET', 'POST'])
+def edit_post(post_id):
+    """edit post"""
+    post = Posts.query.get_or_404(post_id)
+    if request.method == 'POST':
+        post.title = request.form['title']
+        post.content  = request.form['content']
+
+        db.session.commit()
+
+        return redirect(url_for('post_detail', post_id=post_id))
+    return render_template('edit_post.html', post=post)
+
+@app.route('/posts/<int:post_id>/delete', methods=['POST'])
+def delete_post(post_id):
+    """delete post"""
+    post = Posts.query.get_or_404(post_id)
+    db.session.delete(post)
+    db.session.commit()
+
+    return redirect(url_for('user_detail', user_id=post.user_id))
+
+
+
+# TAG ROUTES--------------------------------------------------------------
+
+@app.route('/tags', methods=['GET', 'POST'])
+def tags():
+    """list of tags"""
+    tags = Tags.query.all()
+    return render_template('tags.html', tags=tags)
+
+@app.route('/tags/new', methods=['GET', 'POST'])
+def new_tag():
+    """new tag form"""
+    if request.method == 'POST':
+        name = request.form['name']
+
+        new_tag = Tags(name=name)
+        db.session.add(new_tag)
+        db.session.commit()
+
+        return redirect(url_for('tags'))
+    return render_template('new_tag.html')
+
+@app.route('/tags/<int:tag_id>')
+def tag_detail(tag_id):
+    """tag detail"""
+    tag = Tags.query.get_or_404(tag_id)
+    return render_template('tag_detail.html', tag=tag)
+
+@app.route('/tags/<int:tag_id>/edit', methods=['GET', 'POST'])
+def edit_tag(tag_id):
+    """edit tag"""
+    tag = Tags.query.get_or_404(tag_id)
+    if request.method == 'POST':
+        tag.name = request.form['name']
+
+        db.session.commit()
+
+        return redirect(url_for('tag_detail', tag_id=tag_id))
+    return render_template('edit_tag.html', tag=tag)
+
+@app.route('/tags/<int:tag_id>/delete', methods=['POST'])
+def delete_tag(tag_id):
+    """delete tag"""
+    tag = Tags.query.get_or_404(tag_id)
+    db.session.delete(tag)
+    db.session.commit()
+
+    return redirect(url_for('tags'))
--- models.py
+++ models.py
@@ -1,7 +1,27 @@
-from flask_sqlalchemy import SQLAlchemy 
-from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime 
+
 
 db = SQLAlchemy()
+
+class PostTag(db.Model):
+    """post tags"""
+
+    __tablename__ = "post_tags"
+
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), primary_key=True)
+    tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'), primary_key=True)
+
+class Tags(db.Model):
+    """tags"""
+
+    __tablename__ = "tags"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50), nullable=False, unique=True)
+
+    tag_posts = db.relationship('Posts', secondary='post_tags', back_populates='tags')
+
 
 
 class Users(db.Model):
@@ -14,6 +34,8 @@ class Users(db.Model):
     last_name  = db.Column(db.String(50), nullable=False)
     image_url  = db.Column(db.String(500), nullable=True, default="https://www.freeiconspng.com/uploads/blue-user-icon-32.jpg")
 
+    user_posts = db.relationship('Posts', back_populates='all_users', foreign_keys='Posts.user_id')
+
     def __repr__(self):
         """show info about user"""
         u = self
@@ -25,19 +47,21 @@ class Posts(db.Model):
 
     __tablename__= "posts"
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     title = db.Column(db.String(50), nullable=False)
     content = db.Column(db.Text, nullable=False)
-    # created_at = db.Column(db.DateTime, nullable=False, default=datetime.now())
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now())
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-
-    users = db.relationship('Users', backref='posts')
+   
+    all_users = db.relationship('Users', back_populates='user_posts')
 
     def __repr__(self):
         """show info about post"""
         p = self
         return f"<Post {p.id} {p.title} {p.created_at}>"
     
+
+
 
 def connect_db(app):
     """Connect to database"""
